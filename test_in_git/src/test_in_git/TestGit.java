b72fb2133a6b8c773599f967fe7888f3c7d2c9c7
daiderong <ddr2@foxmail.com>
2015-04-21 14:00:11 +0800

    qwqw

---
+++ test_in_git/src/test_in_git/TestGit.java
@@ -0,0 +1,42 @@
+    /** 
+     * Project Name:test_in_git 
+     * File Name:TestGit.java 
+     * Package Name:test_in_git 
+     * Date:2015年4月21日下午1:56:54 
+     * Copyright (c) 2015, chenzhou1025@126.com All Rights Reserved. 
+     * 
+    */  
+        /** 
+     * Project Name:test_in_git 
+     * File Name:TestGit.java 
+     * Package Name:test_in_git 
+     * Date:2015年4月21日下午1:56:54 
+     * Copyright (c) 2015, drdai@ichronocloud.com All Rights Reserved. 
+     * 
+     */    
+      
+    package test_in_git;  
+    /** 
+ * ClassName:TestGit <br/> 
+ * Function: TODO ADD FUNCTION. <br/> 
+ * Reason:   TODO ADD REASON. <br/> 
+ * Date:     2015年4月21日 下午1:56:54 <br/> 
+ * @author   ddr 
+ * @version   
+ * @since    JDK 1.6 
+ * @see       
+ */
+/** 
+ * ClassName: TestGit <br/> 
+ * Function: TODO ADD FUNCTION. <br/> 
+ * Reason: TODO ADD REASON(可选). <br/> 
+ * date: 2015年4月21日 下午1:56:54 <br/> 
+ * 
+ * @author ddr 
+ * @version  
+ * @since JDK 1.6 
+ */
+public class TestGit {
+
+}
+  
